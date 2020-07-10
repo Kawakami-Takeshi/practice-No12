4dd947b2c5acf363d9baa123604d5338bc5b15f9
”Kawakami-Takeshi” <take.kawakami12@gmail.com>
2020-07-10 06:53:57 +0000

    practice

---
+++ kadai1.php
@@ -0,0 +1,7 @@
+<?php
+$name = "Kawakami";
+if($name == "Kawakami"){
+    echo "私は" . $name . "です";
+}else{
+    echo $name . "ではありません";
+}
---
+++ kadai3.php
@@ -0,0 +1,7 @@
+<?php
+$fruits=array("apple","orange","banana","melon","remon");
+
+foreach($fruits as $fruit){
+    echo $fruit;
+    echo "\n"; //改行
+}
